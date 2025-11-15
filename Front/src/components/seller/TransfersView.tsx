0e3bb6cc735492867238a8b1f04d6fcdbd538cf8
Daniel Rodriguez <dani.s.rodriguez.r@gmail.com>
2025-11-15 16:56:54 -0500

    Eliminating a number

--- Front/src/components/seller/TransfersView.tsx
+++ Front/src/components/seller/TransfersView.tsx
@@ -803,7 +803,7 @@ export const TransfersView: React.FC<TransfersViewProps> = ({
               className="flex-1 md:flex-none text-xs md:text-sm"
             >
               <CheckCircle className="h-3 w-3 md:h-4 md:w-4 mr-1 md:mr-2" />
-              Completadas ({completedTransfers.length})
+              Completadas 
             </Button>
           </div>
         </CardContent>
